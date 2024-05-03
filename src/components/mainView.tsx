be13b2ae3cc6d82e8cc40ec3514e29180a57a5ca
piechota <piechota33@gmail.com>
2024-05-03 10:45:01 +0200

    save tree state and scrollbar

--- src/components/mainView.tsx
+++ src/components/mainView.tsx
@@ -6,6 +6,7 @@ import './css/mainView.css'
 
 import { authGlobal, AUTH_DISABLED } from '../system/authentication';
 import { FunctionComponent, useState } from 'react';
+import useResizeObserver from 'use-resize-observer';
 
 type Props = {
   changeAuthButtonState: (state: number) => void;
@@ -14,17 +15,18 @@ type Props = {
 export const MainView: FunctionComponent<Props> = ({changeAuthButtonState}) => {
 
   const [selectedFile, setSelectedFile] = useState<string>('');
+  const { ref: toolbarRef, height: toolbarHeight = 1 } = useResizeObserver<HTMLDivElement>(); 
+
 
   const handleLogOutClick = () => {
     authGlobal.logout().then( () => { changeAuthButtonState(AUTH_DISABLED) } );
 };
-
   return  (
     <div style={{height: '100%'}}>
-      <div className='toolbox-view'>
+      <div ref={toolbarRef} className='toolbox-view'>
         <span><button type='button' onClick={handleLogOutClick}>Log out</button></span>
       </div>
-      <div className='main-view'>
+      <div style={{height: `calc(100% - ${toolbarHeight}px)`}} className='main-view'>
         <div className='tree-view'>
           <TreeView setSelectedFile={setSelectedFile}/>
         </div>
