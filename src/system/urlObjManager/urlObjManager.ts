97766554ec406dd3003fa1de447fa8ee49203d89
piechota <piechota33@gmail.com>
2024-05-30 21:53:46 +0200

    fix import

--- src/system/urlObjManager/urlObjManager.ts
+++ src/system/urlObjManager/urlObjManager.ts
@@ -2,11 +2,13 @@ import { WebWorkerManager } from "@/interfaces/webWorker";
 import { UrlObjManagerThreadInterface } from "./worker";
 import { UrlObjManagerInterface, UrlObjManagerThreadInterfaceType } from "./workerShared";
 
+import workerURL from './worker?worker&url';
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UrlObjManager extends UrlObjManagerInterface {}
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UrlObjManager extends WebWorkerManager<UrlObjManagerThreadInterfaceType> {
     constructor() {
-        super(new URL('./worker.ts', import.meta.url), UrlObjManagerThreadInterface);
+        super(workerURL, UrlObjManagerThreadInterface);
     }
 }
