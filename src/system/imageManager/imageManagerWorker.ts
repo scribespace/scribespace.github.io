ab0feadf2f8c94752f706b160bc9ccf4608b4845
piechota <piechota33@gmail.com>
2024-06-08 23:49:38 +0200

    move to promise, didn't change anything

--- src/system/imageManager/imageManagerWorker.ts
+++ src/system/imageManager/imageManagerWorker.ts
@@ -1,13 +1,16 @@
 import { WebWorkerThread } from "@/interfaces/webWorker";
 
 export const ImageManagerWorkerPublic = {
-  blobsToUrlObjs(blobs: Blob[]): [urlObjs: string[]] {
-    const urlObjs: string[] = [];
-    for (const blob of blobs) {
-      urlObjs.push(URL.createObjectURL(blob));
-    }
-
-    return [urlObjs]; 
+  blobsToUrlObjs(blobs: Blob[]): Promise<[urlsObjs:string[]]> {
+    return new Promise<[urlsObjs:string[]]>(
+      (resolve) => {
+        const urlObjs: string[] = [];
+        for (const blob of blobs) {
+          urlObjs.push(URL.createObjectURL(blob));
+        }
+        resolve([urlObjs]); 
+      }
+    );
   },
   async preloadImage(src: string): Promise<void> {
     // disable CORS, still can't load image to memoery but it ends up in cache
